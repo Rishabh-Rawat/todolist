5a5ff4fff7a5de049cc4772ffe9ad62d4d152e2c
Rishabh Rawat <rishabhrawat.rishu@gmail.com>
2023-01-29 02:06:50 +0530

    updated mongoDB URI environment variable

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_DB_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
